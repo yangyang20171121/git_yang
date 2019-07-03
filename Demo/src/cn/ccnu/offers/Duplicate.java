dfc8b1f19c8d6a5fcab0ea666483c6cbb71b17d2
yangyang <838438890@qq.com>
2019-07-03 20:11:27 +0800

    scanner

--- Demo/src/cn/ccnu/offers/Duplicate.java
+++ Demo/src/cn/ccnu/offers/Duplicate.java
@@ -1,8 +1,14 @@
 package cn.ccnu.offers;
 
+import java.util.Scanner;
+
 public class Duplicate {
 	public static void main(String[] args){
-		int [] num={2,3,1,0,2,5,3};
+		Scanner sc=new Scanner(System.in);
+		String [] str=sc.nextLine().split(" ");
+		int [] num=new int[str.length];
+		for(int i=0;i<str.length;i++)
+			num[i]=Integer.parseInt(str[i]);
 		int m=getDuplicate(num);
 		System.out.println(m);
 	}
